f6694e55d779dc3232dc54fc1a5c06816ab7ce10
Malang00 <91902331+Malang00@users.noreply.github.com>
2023-10-13 14:59:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ bot = Client("bot",
 
 @bot.on_message(filters.command(["start"]) & filters.chat(ADMINS))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"Hello 👋 **I am a simple video downloader bot**.\n\n**Developer** : 𝐉𝐚𝐚𝐭\n**Language** : Python\n**Framework** : Pyrogram\n\n/jaat - **To download from TXT file.**")
+    editable = await m.reply_text(f"Hello 👋 **I am a simple video downloader bot**.\n\n**Developer** : 𝐘𝐨𝐮𝐫 𝐃𝐚𝐝𝐝𝐲\n**Language** : Python\n**Framework** : Pyrogram\n\n/Txt - **To download from TXT file.**")
 
 
 @bot.on_message(filters.command("stop") & filters.chat(ADMINS))
@@ -39,7 +39,7 @@ async def restart_handler(_, m):
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-@bot.on_message(filters.command(["Jaat"]) & filters.chat(ADMINS))
+@bot.on_message(filters.command(["Txt"]) & filters.chat(ADMINS))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text(f"**Hello Bruh, I am Txt Downloader Bot.\nSend txt file**")
     input: Message = await bot.listen(editable.chat.id)
@@ -169,8 +169,8 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:                               
-                cc = f'** {str(count).zfill(3)}.** {name1} By Jaat ({res}) \n\n**Batch Name :** {b_name}\n\n**Downloaded By : {CR}**'
-                cc1 = f'** {str(count).zfill(3)}.** {name1} By Jaat\n\n**Batch Name :** {b_name}\n\n**Downloaded By : {CR}**'
+                cc = f'** {str(count).zfill(3)}.** {name1} ({res}) \n\n**Batch Name :** {b_name}\n\n**Downloaded By : {CR}**'
+                cc1 = f'** {str(count).zfill(3)}.** {name1} \n\n**Batch Name :** {b_name}\n\n**Downloaded By : {CR}**'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
@@ -185,19 +185,19 @@ async def account_login(bot: Client, m: Message):
                         continue
                 elif ".pdf" in url:
                     try:
-                        cmd = f'yt-dlp -o "{name} Jaat.pdf" "{url}"'
+                        cmd = f'yt-dlp -o "{name} .pdf" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=f'{name} Jaat.pdf', caption=cc1)
+                        copy = await bot.send_document(chat_id=m.chat.id,document=f'{name} .pdf', caption=cc1)
                         await copy.copy(chat_id = -1001812808410)
                         count += 1
-                        os.remove(f'{name} Jaat.pdf')
+                        os.remove(f'{name} .pdf')
                     except FloodWait as e:
                         await m.reply_text(str(e))
                         time.sleep(e.x)
                         continue
                 else:
-                    prog = await m.reply_text(f"**Downloading Video**\n**Video Name :** `{name}\nQuality : {raw_text2}`\n**link : **`{url}`\n\n **⌈ 𝐌𝐚𝐝𝐞 𝐁𝐲 𝐉𝐀𝐀𝐓 ⌋**")
+                    prog = await m.reply_text(f"**Downloading Video**\n**Video Name :** `{name}\nQuality : {raw_text2}`\n**link : **`{url}`\n\n **⌈ 𝐌𝐚𝐝𝐞 𝐁𝐲 𝐘𝐨𝐮𝐫 𝐃𝐚𝐝𝐝𝐲 ⌋**")
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
                     await prog.delete(True)
